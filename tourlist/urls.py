65108b6f70d1b9f52e7cfcee571899035f4c5a8c
jongw0o0 <127378236+jongw0o0@users.noreply.github.com>
2023-07-13 13:29:25 +0900

    test0713_1

--- tourlist/urls.py
+++ tourlist/urls.py
@@ -6,8 +6,8 @@ app_name = 'tourlist'
 
 urlpatterns = [
     # path('', views.index, name='index1'),
-    path('1/', views.index1, name='index1'),
-    path('/2/', views.index2, name='index2'),
-    path('/3/', views.index3, name='index3'),
+    path('index1/', views.index1, name='index1'),
+    path('index2/', views.index2, name='index2'),
+    path('index3/', views.index3, name='index3'),
     # 상세정보 url 필요할듯
 ]
