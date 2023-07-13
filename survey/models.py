bcaeba7940bb9e5fb71aed9358f25c18e919ebd3
jongw0o0 <127378236+jongw0o0@users.noreply.github.com>
2023-07-13 16:39:46 +0900

    test0713_4

--- survey/models.py
+++ survey/models.py
@@ -2,6 +2,61 @@ from django.db import models
 
 # Create your models here.
 
+class Survey(models.Model):
+    GENDER_CHOICES = (
+        ('M', '남'),
+        ('F', '여'),
+    )
+
+    AGE_CHOICES = (
+        ('1', '10대'),
+        ('2', '20대'),
+        ('3', '30대'),
+        ('4', '40대'),
+        ('5', '50대'),
+        ('6', '60대 이상'),
+    )
+
+    REGION_CHOICES = (
+        ('서울특별시', '서울특별시'),
+        ('경기도', '경기도'),
+        ('인천광역시', '인천광역시'),
+        ('강원도', '강원도'),
+    )
+
+    AREA_CHOICES = (
+        ('강남구', '강남구'),
+        ('성동구', '성동구'),
+        ('종로구', '종로구'),
+    )
+
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    age = models.CharField(max_length=1,choices=AGE_CHOICES)
+    region_1 = models.CharField(max_length=30, choices=REGION_CHOICES)
+    area_1 = models.CharField(max_length=30, choices=AREA_CHOICES)
+    region_2 = models.CharField(max_length=30, choices=REGION_CHOICES, blank=True)
+    area_2 = models.CharField(max_length=30, choices=AREA_CHOICES, blank=True)
+    region_3 = models.CharField(max_length=30, choices=REGION_CHOICES, blank=True)
+    area_3 = models.CharField(max_length=30, choices=AREA_CHOICES, blank=True)    
+
+    def save(self, *args, **kwargs):
+        self.myarea = f"{self.region_1} {self.area_1}"  # Set the value of myarea
+        super().save(*args, **kwargs)
+
+
+
+    myarea_1 = models.CharField(max_length=60)
+    myarea_2 = models.CharField(max_length=60, blank=True)
+    myarea_3 = models.CharField(max_length=60, blank=True)
+
+    
+    def regionsave(self, *args, **kwargs):
+        self.myarea_1 = self.region_1 + ' ' + self.area_1
+        self.myarea_2 = self.region_2 + ' ' + self.area_2
+        self.myarea_3 = self.region_3 + ' ' + self.area_3
+        super(Survey, self).save(*args, **kwargs)
+
+
 # class surveyinput(models.Model):
 #     gender = models.CharField('성별', max_length=1)
 #     age = models.CharField('연령', max_length=1)
--- survey/views.py
+++ survey/views.py
@@ -1,11 +1,53 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import SurveyForm
+from .models import Survey
 
 # Create your views here.
 
+# def index(request):
+#     return render(request, 'survey/survey.html')
+
 def index(request):
-    return render(request, 'survey/survey.html')
+    if request.method == 'POST':
+        form = SurveyForm(request.POST)
+       
+        if form.is_valid():
+            gender = form.cleaned_data['gender']
+            age = form.cleaned_data['age']
+            region_1 = form.cleaned_data['region_1']
+            area_1 = form.cleaned_data['area_1']
+            myarea = region_1 + ' ' + area_1
+            # region_2 = form.cleaned_data['region_2']
+            # area_2 = form.cleaned_data['area_2']
+            # region_3 = form.cleaned_data['region_3']
+            # area_3 = form.cleaned_data['area_3']
+            
+            survey_instance = Survey(
+                gender=gender,
+                age=age,
+                region_1=region_1,
+                area_1=area_1,
+                myarea = myarea
+                # region_2=region_2,
+                # area_2=area_2
+                # region_3=region_3,
+                # area_3=area_3
+            )
+            # survey_instance.save()
+            survey_instance.regionsave()
+ 
+            return redirect('success_url')  # 저장 후 리다이렉트할 URL
+    else:
+        form = SurveyForm()
+    
+    return render(request, 'survey/survey.html', {'form': form})
+
+
+
+
+
 
 
 # def api(request):
