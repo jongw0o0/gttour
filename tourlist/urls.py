52f5377aa95ad95d39b3dcc5143b1e13858f67c5
jongw0o0 <127378236+jongw0o0@users.noreply.github.com>
2023-07-24 16:54:24 +0900

    test0724_3

--- tourlist/urls.py
+++ tourlist/urls.py
@@ -8,5 +8,17 @@ urlpatterns = [
     # path('', views.index, name='index1'),
     path('similadest_view/', views.similadest_view, name='similadest_view'),
     path('destination_view/', views.destination_view, name='destination_view'),
+
+    path('similadest_view/list1/', views.simlist1, name='simlist1'),
+    path('similadest_view/list2/', views.simlist2, name='simlist2'),
+    path('similadest_view/list3/', views.simlist3, name='simlist3'),
+    path('similadest_view/list4/', views.simlist4, name='simlist4'),
+    path('similadest_view/list5/', views.simlist5, name='simlist5'),
+
+    # path('destination_view/list1/', views.deslist1, name='deslist1'),
+    # path('destination_view/list2/', views.deslist2, name='deslist2'),
+    # path('destination_view/list3/', views.deslist3, name='deslist3'),
+    # path('destination_view/list4/', views.deslist4, name='deslist4'),
+    # path('destination_view/list5/', views.deslist5, name='deslist5'),
     # 상세정보 url 필요할듯
 ]
--- tourlist/views.py
+++ tourlist/views.py
@@ -46,11 +46,142 @@ def destination_view(request):
                 'negative' : db_object.negative,
                 'image': db_object.image.url, 
                 'tour_loc': db_object.tour_loc,
+                'url': f'/tourlist/destination_view/{db_object.location}/'
             })
         except Tourmodel.DoesNotExist:
             pass
-        
+
     with open('destination_data_list.json', 'w') as json_file:
         json.dump(destination_data_list, json_file)
     print('7',destination_data_list)
     return render(request, 'tourlist/destination.html', {'destination_data_list': destination_data_list})
+
+
+
+
+
+
+def simlist1(request):
+    with open('./similadest.csv', 'r') as csvfile:
+        csv_reader = csv.reader(csvfile)
+        similadest_data = list(csv_reader)
+    print('5',similadest_data)
+    similadest_data_list = []  
+    for json_value in similadest_data:
+        location = json_value[0]    
+        try:
+            db_object = Tourmodel.objects.get(location=location)
+            similadest_data_list.append({
+                'location': db_object.location,
+                'positive': db_object.positive,
+                'negative' : db_object.negative,
+                'image': db_object.image.url, 
+                'tour_loc': db_object.tour_loc,
+            })
+        except Tourmodel.DoesNotExist:
+            pass
+        
+    with open('similadest_data_list.json', 'w') as json_file:
+        json.dump(similadest_data_list, json_file)
+    print('6',similadest_data_list)
+    return render(request, 'tourlist/simlist1.html', {'similadest_data_list': similadest_data_list})
+
+def simlist2(request):
+    with open('./similadest.csv', 'r') as csvfile:
+        csv_reader = csv.reader(csvfile)
+        similadest_data = list(csv_reader)
+    print('5',similadest_data)
+    similadest_data_list = []  
+    for json_value in similadest_data:
+        location = json_value[0]    
+        try:
+            db_object = Tourmodel.objects.get(location=location)
+            similadest_data_list.append({
+                'location': db_object.location,
+                'positive': db_object.positive,
+                'negative' : db_object.negative,
+                'image': db_object.image.url, 
+                'tour_loc': db_object.tour_loc,
+            })
+        except Tourmodel.DoesNotExist:
+            pass
+        
+    with open('similadest_data_list.json', 'w') as json_file:
+        json.dump(similadest_data_list, json_file)
+    print('6',similadest_data_list)
+    return render(request, 'tourlist/simlist1.html', {'similadest_data_list': similadest_data_list})
+
+def simlist3(request):
+    with open('./similadest.csv', 'r') as csvfile:
+        csv_reader = csv.reader(csvfile)
+        similadest_data = list(csv_reader)
+    print('5',similadest_data)
+    similadest_data_list = []  
+    for json_value in similadest_data:
+        location = json_value[0]    
+        try:
+            db_object = Tourmodel.objects.get(location=location)
+            similadest_data_list.append({
+                'location': db_object.location,
+                'positive': db_object.positive,
+                'negative' : db_object.negative,
+                'image': db_object.image.url, 
+                'tour_loc': db_object.tour_loc,
+            })
+        except Tourmodel.DoesNotExist:
+            pass
+        
+    with open('similadest_data_list.json', 'w') as json_file:
+        json.dump(similadest_data_list, json_file)
+    print('6',similadest_data_list)
+    return render(request, 'tourlist/simlist1.html', {'similadest_data_list': similadest_data_list})
+
+def simlist4(request):
+    with open('./similadest.csv', 'r') as csvfile:
+        csv_reader = csv.reader(csvfile)
+        similadest_data = list(csv_reader)
+    print('5',similadest_data)
+    similadest_data_list = []  
+    for json_value in similadest_data:
+        location = json_value[0]    
+        try:
+            db_object = Tourmodel.objects.get(location=location)
+            similadest_data_list.append({
+                'location': db_object.location,
+                'positive': db_object.positive,
+                'negative' : db_object.negative,
+                'image': db_object.image.url, 
+                'tour_loc': db_object.tour_loc,
+            })
+        except Tourmodel.DoesNotExist:
+            pass
+        
+    with open('similadest_data_list.json', 'w') as json_file:
+        json.dump(similadest_data_list, json_file)
+    print('6',similadest_data_list)
+    return render(request, 'tourlist/simlist1.html', {'similadest_data_list': similadest_data_list})
+
+def simlist5(request):
+    with open('./similadest.csv', 'r') as csvfile:
+        csv_reader = csv.reader(csvfile)
+        similadest_data = list(csv_reader)
+    print('5',similadest_data)
+    similadest_data_list = []  
+    for json_value in similadest_data:
+        location = json_value[0]    
+        try:
+            db_object = Tourmodel.objects.get(location=location)
+            similadest_data_list.append({
+                'location': db_object.location,
+                'positive': db_object.positive,
+                'negative' : db_object.negative,
+                'image': db_object.image.url, 
+                'tour_loc': db_object.tour_loc,
+            })
+        except Tourmodel.DoesNotExist:
+            pass
+        
+    with open('similadest_data_list.json', 'w') as json_file:
+        json.dump(similadest_data_list, json_file)
+    print('6',similadest_data_list)
+    return render(request, 'tourlist/simlist1.html', {'similadest_data_list': similadest_data_list})
