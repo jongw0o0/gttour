a03cb17b2f13146adeb3e2f0a1d4489ff9e404ed
jongw0o0 <127378236+jongw0o0@users.noreply.github.com>
2023-07-25 11:07:21 +0900

    test0725_2 인코딩 수정

--- classification/views.py
+++ classification/views.py
@@ -12,13 +12,13 @@ def index(request):
         csv_reader = csv.reader(csvfile)
         destination_data = list(csv_reader)
         with open('destination_data.json', 'w') as json_file:
-            json.dump(destination_data, json_file)
+            json.dump(destination_data, json_file).encode("utf-8")
 
     with open(csv_file_path_similadest, 'r') as csvfile:
         csv_reader = csv.reader(csvfile)
         similadest_data = list(csv_reader)
         with open('similadest_data.json', 'w') as json_file:
-            json.dump(similadest_data, json_file)
+            json.dump(similadest_data, json_file).encode("utf-8")
 
     print('1',destination_data,'2',similadest_data) # 콘솔 확인용
 
