9398479444bd560cf69d0d94009dde62919d57fe
jongw0o0 <127378236+jongw0o0@users.noreply.github.com>
2023-07-25 11:16:35 +0900

    test0725_4 인코딩 수정3

--- classification/views.py
+++ classification/views.py
@@ -12,13 +12,13 @@ def index(request):
     with open(csv_file_path_destination, 'r', encoding='utf-8') as csvfile:
         csv_reader = csv.reader(csvfile)
         destination_data = list(csv_reader)
-        with open('destination_data.json', 'w') as json_file:
-            json.dump(destination_data, json_file).encode("utf-8")
+        with open('destination_data.json', 'w', encoding='utf-8') as json_file:
+            json.dump(destination_data, json_file)
 
     with open(csv_file_path_similadest, 'r',  encoding='utf-8') as csvfile:
         csv_reader = csv.reader(csvfile)
         similadest_data = list(csv_reader)
-        with open('similadest_data.json', 'w') as json_file:
+        with open('similadest_data.json', 'w', encoding='utf-8') as json_file:
             json.dump(similadest_data, json_file).encode("utf-8")
 
     print('1',destination_data,'2',similadest_data) # 콘솔 확인용
