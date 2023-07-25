521552447183324c2d31d9fd6971e30a997e7f86
jongw0o0 <127378236+jongw0o0@users.noreply.github.com>
2023-07-25 17:30:51 +0900

    test0725_13

--- classification/views.py
+++ classification/views.py
@@ -9,17 +9,18 @@ def index(request):
     csv_file_path_destination = './destination.csv'
     csv_file_path_similadest = './similadest.csv'
 
+    with open(csv_file_path_similadest, 'r',  encoding='cp949') as csvfile:
+        csv_reader = csv.reader(csvfile)
+        similadest_data = list(csv_reader)
+        with open('similadest_data.json', 'w', encoding='cp949') as json_file:
+            json.dump(similadest_data, json_file)
+
     with open(csv_file_path_destination, 'r', encoding='cp949') as csvfile:
         csv_reader = csv.reader(csvfile)
         destination_data = list(csv_reader)
         with open('destination_data.json', 'w', encoding='cp949') as json_file:
             json.dump(destination_data, json_file)
 
-    with open(csv_file_path_similadest, 'r',  encoding='cp949') as csvfile:
-        csv_reader = csv.reader(csvfile)
-        similadest_data = list(csv_reader)
-        with open('similadest_data.json', 'w', encoding='cp949') as json_file:
-            json.dump(similadest_data, json_file)
 
     print('1',destination_data,'2',similadest_data) # 콘솔 확인용
 
