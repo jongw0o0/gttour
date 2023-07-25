63afff7782bcb6346388a05795b428007a77948b
jongw0o0 <127378236+jongw0o0@users.noreply.github.com>
2023-07-25 11:19:18 +0900

    test0725_6 인코딩 수정5

--- classification/views.py
+++ classification/views.py
@@ -9,17 +9,17 @@ def index(request):
     csv_file_path_destination = './destination.csv'
     csv_file_path_similadest = './similadest.csv'
 
-    with open(csv_file_path_destination, 'r', encoding='utf-8') as csvfile:
-        csv_reader = csv.reader(csvfile, encoding='utf-8')
+    with open(csv_file_path_destination, 'r', encoding='cp949') as csvfile:
+        csv_reader = csv.reader(csvfile)
         destination_data = list(csv_reader)
-        with open('destination_data.json', 'w', encoding='utf-8') as json_file:
+        with open('destination_data.json', 'w', encoding='cp949') as json_file:
             json.dump(destination_data, json_file)
 
-    with open(csv_file_path_similadest, 'r',  encoding='utf-8') as csvfile:
+    with open(csv_file_path_similadest, 'r',  encoding='cp949') as csvfile:
         csv_reader = csv.reader(csvfile)
         similadest_data = list(csv_reader)
-        with open('similadest_data.json', 'w', encoding='utf-8') as json_file:
-            json.dump(similadest_data, json_file).encode("utf-8")
+        with open('similadest_data.json', 'w', encoding='cp949') as json_file:
+            json.dump(similadest_data, json_file)
 
     print('1',destination_data,'2',similadest_data) # 콘솔 확인용
 
