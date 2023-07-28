e425341c9eb6fa19cedaa6c1dfac15c6be22e634
jongw0o0 <127378236+jongw0o0@users.noreply.github.com>
2023-07-28 16:57:26 +0900

    test0728_7

--- tourlist/views.py
+++ tourlist/views.py
@@ -1,56 +1,39 @@
 from django.shortcuts import render
 from .models import Tourmodel
 import json
+from django.http import JsonResponse
 
-def read_json_file(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        data = json.load(file)
-    return data
+# def read_json_file(file_path):
+#     with open(file_path, 'r', encoding='utf-8') as file:
+#         data = json.load(file)
+#     return data
+
 
 def surveydest_view(request):
-    
-    survey_data_file_path = './survey_data.json'
-    survey_data = read_json_file(survey_data_file_path)
+    with open('./survey_data.json', 'r', encoding='utf-8') as json_file:
+        survey_data_data = json.load(json_file)    
+   
+    survey_data_list = []  
 
-    data = {
-        'survey_data': survey_data, 
-    }
-
-    return render(request, 'tourlist/surveydest.html', {'data': data})
-
-def surveylist1(request):
-    
-    survey_data_file_path = './survey_data.json'
-    survey_data = read_json_file(survey_data_file_path)
-
-    data = {
-        'survey_data': survey_data, 
-    }
-
-    return render(request, 'tourlist/surveylist1.html', {'data': data})
-
-def surveylist2(request):
-    
-    survey_data_file_path = './survey_data.json'
-    survey_data = read_json_file(survey_data_file_path)
-
-    data = {
-        'survey_data': survey_data, 
-    }
-
-    return render(request, 'tourlist/surveylist2.html', {'data': data})
-
-def surveylist3(request):
-    
-    survey_data_file_path = './survey_data.json'
-    survey_data = read_json_file(survey_data_file_path)
-
-    data = {
-        'survey_data': survey_data, 
-    }
-
-    return render(request, 'tourlist/surveylist3.html', {'data': data})
-
+    for location in [survey_data_data['myarea_1'], survey_data_data['myarea_2'], survey_data_data['myarea_3']]:
+        try:
+            db_object = Tourmodel.objects.get(location=location)
+            survey_data_list.append({
+                'location': db_object.location,
+                'positive': db_object.positive,
+                'negative': db_object.negative,
+                'image': db_object.image.url,
+                'tour_loc_1': db_object.tour_loc_1,
+                'tour_loc_2': db_object.tour_loc_2,
+                'tour_loc_3': db_object.tour_loc_3,
+            })
+        except Tourmodel.DoesNotExist:
+            pass
+        
+    with open('survey_data_list.json', 'w', encoding='utf-8') as json_file:
+        json.dump(survey_data_list, json_file)
+ 
+    return render(request, 'tourlist/surveydest.html', {'survey_data_list': survey_data_list})
 
 
 
@@ -83,7 +66,6 @@ def similadest_view(request):
     return render(request, 'tourlist/similadest.html', {'similadest_data_list': similadest_data_list})
 
 
-
 def destination_view(request):
     with open('./destination.json', 'r', encoding='utf-8') as json_file:
         destination_data = json.load(json_file)
@@ -110,10 +92,83 @@ def destination_view(request):
     # print('7',destination_data_list)
     return render(request, 'tourlist/destination.html', {'destination_data_list': destination_data_list})
 
+def surveylist1(request):
+    with open('./survey_data.json', 'r', encoding='utf-8') as json_file:
+        survey_data_data = json.load(json_file)    
+   
+    survey_data_list = []  
 
+    for location in [survey_data_data['myarea_1'], survey_data_data['myarea_2'], survey_data_data['myarea_3']]:
+        try:
+            db_object = Tourmodel.objects.get(location=location)
+            survey_data_list.append({
+                'location': db_object.location,
+                'positive': db_object.positive,
+                'negative': db_object.negative,
+                'image': db_object.image.url,
+                'tour_loc_1': db_object.tour_loc_1,
+                'tour_loc_2': db_object.tour_loc_2,
+                'tour_loc_3': db_object.tour_loc_3,
+            })
+        except Tourmodel.DoesNotExist:
+            pass
+        
+    with open('survey_data_list.json', 'w', encoding='utf-8') as json_file:
+        json.dump(survey_data_list, json_file)
+ 
+    return render(request, 'tourlist/surveylist1.html', {'survey_data_list': survey_data_list})
 
+def surveylist2(request):
+    with open('./survey_data.json', 'r', encoding='utf-8') as json_file:
+        survey_data_data = json.load(json_file)    
+   
+    survey_data_list = []  
 
+    for location in [survey_data_data['myarea_1'], survey_data_data['myarea_2'], survey_data_data['myarea_3']]:
+        try:
+            db_object = Tourmodel.objects.get(location=location)
+            survey_data_list.append({
+                'location': db_object.location,
+                'positive': db_object.positive,
+                'negative': db_object.negative,
+                'image': db_object.image.url,
+                'tour_loc_1': db_object.tour_loc_1,
+                'tour_loc_2': db_object.tour_loc_2,
+                'tour_loc_3': db_object.tour_loc_3,
+            })
+        except Tourmodel.DoesNotExist:
+            pass
+        
+    with open('survey_data_list.json', 'w', encoding='utf-8') as json_file:
+        json.dump(survey_data_list, json_file)
+ 
+    return render(request, 'tourlist/surveylist2.html', {'survey_data_list': survey_data_list})
 
+def surveylist3(request):
+    with open('./survey_data.json', 'r', encoding='utf-8') as json_file:
+        survey_data_data = json.load(json_file)    
+   
+    survey_data_list = []  
+
+    for location in [survey_data_data['myarea_1'], survey_data_data['myarea_2'], survey_data_data['myarea_3']]:
+        try:
+            db_object = Tourmodel.objects.get(location=location)
+            survey_data_list.append({
+                'location': db_object.location,
+                'positive': db_object.positive,
+                'negative': db_object.negative,
+                'image': db_object.image.url,
+                'tour_loc_1': db_object.tour_loc_1,
+                'tour_loc_2': db_object.tour_loc_2,
+                'tour_loc_3': db_object.tour_loc_3,
+            })
+        except Tourmodel.DoesNotExist:
+            pass
+        
+    with open('survey_data_list.json', 'w', encoding='utf-8') as json_file:
+        json.dump(survey_data_list, json_file)
+ 
+    return render(request, 'tourlist/surveylist3.html', {'survey_data_list': survey_data_list})
 
 def simlist1(request):
     with open('./similadest.json', 'r', encoding='utf-8') as json_file:
@@ -140,7 +195,7 @@ def simlist1(request):
         
     with open('similadest_data_list.json', 'w', encoding='utf-8') as json_file:
         json.dump(similadest_data_list, json_file)
-    # print('6',similadest_data_list)
+
     return render(request, 'tourlist/simlist1.html', {'similadest_data_list': similadest_data_list})
 
 def simlist2(request):
@@ -168,7 +223,7 @@ def simlist2(request):
         
     with open('similadest_data_list.json', 'w', encoding='utf-8') as json_file:
         json.dump(similadest_data_list, json_file)
-    # print('6',similadest_data_list)
+
     return render(request, 'tourlist/simlist2.html', {'similadest_data_list': similadest_data_list})
 
 def simlist3(request):
@@ -196,7 +251,7 @@ def simlist3(request):
         
     with open('similadest_data_list.json', 'w', encoding='utf-8') as json_file:
         json.dump(similadest_data_list, json_file)
-    # print('6',similadest_data_list)
+
     return render(request, 'tourlist/simlist3.html', {'similadest_data_list': similadest_data_list})
 
 def simlist4(request):
@@ -224,7 +279,7 @@ def simlist4(request):
         
     with open('similadest_data_list.json', 'w', encoding='utf-8') as json_file:
         json.dump(similadest_data_list, json_file)
-    # print('6',similadest_data_list)
+
     return render(request, 'tourlist/simlist4.html', {'similadest_data_list': similadest_data_list})
 
 def simlist5(request):
@@ -252,7 +307,7 @@ def simlist5(request):
         
     with open('similadest_data_list.json', 'w', encoding='utf-8') as json_file:
         json.dump(similadest_data_list, json_file)
-    # print('6',similadest_data_list)
+ 
     return render(request, 'tourlist/simlist5.html', {'similadest_data_list': similadest_data_list})
 
 
@@ -277,10 +332,10 @@ def deslist1(request):
             })
         except Tourmodel.DoesNotExist:
             pass
-        
-    with open('destination_data_list.json', 'w') as json_file:
+
+    with open('destination_data_list.json', 'w', encoding='utf-8') as json_file:
         json.dump(destination_data_list, json_file)
-    # print('7',destination_data_list)
+
     return render(request, 'tourlist/deslist1.html', {'destination_data_list': destination_data_list})
 
 def deslist2(request):
@@ -303,10 +358,10 @@ def deslist2(request):
             })
         except Tourmodel.DoesNotExist:
             pass
-        
-    with open('destination_data_list.json', 'w') as json_file:
+
+    with open('destination_data_list.json', 'w', encoding='utf-8') as json_file:
         json.dump(destination_data_list, json_file)
-    # print('7',destination_data_list)
+
     return render(request, 'tourlist/deslist2.html', {'destination_data_list': destination_data_list})
 
 def deslist3(request):
@@ -329,10 +384,10 @@ def deslist3(request):
             })
         except Tourmodel.DoesNotExist:
             pass
-        
-    with open('destination_data_list.json', 'w') as json_file:
+
+    with open('destination_data_list.json', 'w', encoding='utf-8') as json_file:
         json.dump(destination_data_list, json_file)
-    # print('7',destination_data_list)
+
     return render(request, 'tourlist/deslist3.html', {'destination_data_list': destination_data_list})
 
 def deslist4(request):
@@ -355,10 +410,10 @@ def deslist4(request):
             })
         except Tourmodel.DoesNotExist:
             pass
-        
-    with open('destination_data_list.json', 'w') as json_file:
+
+    with open('destination_data_list.json', 'w', encoding='utf-8') as json_file:
         json.dump(destination_data_list, json_file)
-    # print('7',destination_data_list)
+
     return render(request, 'tourlist/deslist4.html', {'destination_data_list': destination_data_list})
 
 def deslist5(request):
@@ -381,8 +436,9 @@ def deslist5(request):
             })
         except Tourmodel.DoesNotExist:
             pass
-        
-    with open('destination_data_list.json', 'w') as json_file:
+
+    with open('destination_data_list.json', 'w', encoding='utf-8') as json_file:
         json.dump(destination_data_list, json_file)
-    # print('7',destination_data_list)
+
     return render(request, 'tourlist/deslist5.html', {'destination_data_list': destination_data_list})
+
